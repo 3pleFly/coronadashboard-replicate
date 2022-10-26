73787b2871b93a8748d989d08d62eb13277ee807
3plefly <3plefly@gmail.com>
2022-10-26 14:34:04 +0300

    first graph complete

--- src/app/shared/components/charts/hospitalized-chart/hospitalized-chart.component.ts
+++ src/app/shared/components/charts/hospitalized-chart/hospitalized-chart.component.ts
@@ -2,6 +2,26 @@ import { Component, Input, OnInit } from '@angular/core';
 import { DataApiService } from 'src/app/shared/services/data-api.service';
 import * as Highcharts from 'highcharts';
 
+Highcharts.setOptions({
+  lang: {
+    shortMonths: [
+      'ינואר',
+      'פברואר',
+      'מרץ',
+      'אפריל',
+      'מאי',
+      'יוני',
+      'יולי',
+      'אוגוסט',
+      'ספטמבר',
+      'אוקטובר',
+      'נובמבר',
+      'דצמבר',
+    ],
+    weekdays: ['ראשון', 'שני', 'שלישי', 'רביעי', 'חמישי', 'שישי', 'שבת']
+  },
+});
+
 @Component({
   selector: 'app-hospitalized-chart',
   templateUrl: './hospitalized-chart.component.html',
@@ -10,21 +30,74 @@ import * as Highcharts from 'highcharts';
 export class HospitalizedChartComponent implements OnInit {
   @Input() width!: string;
   @Input() height!: string;
-
+  data = [];
+  dates: number[] = [];
+  lightSick: any[] = [];
+  mediumSick: any[] = [];
+  harshSick: any[] = [];
+  updateFlag = false;
   Highcharts: typeof Highcharts = Highcharts;
+
   chartOptions: Highcharts.Options = {
+    title: {
+      text: 'מספר מאושפזים יומי',
+    },
     series: [
       {
-        data: [1, 2, 3],
-        type: 'line',
+        name: 'חולים קל',
+        data: this.lightSick,
+        type: 'area',
+      },
+      {
+        name: 'חולים בינוני',
+        data: this.mediumSick,
+        type: 'area',
+      },
+      {
+        name: 'חולים קשה',
+        data: this.harshSick,
+        type: 'area',
       },
     ],
     chart: {
-      styledMode: true
-     }
+      styledMode: true,
+    },
+    yAxis: {
+      title: {
+        text: 'מספר מאושפזים',
+      },
+    },
+    xAxis: {
+      type: 'datetime',
+      labels: {
+        format: '{value:%e-%b}',
+      },
+    },
+    plotOptions: {
+      area: {
+        stacking: 'normal',
+      },
+    },
   };
 
   constructor(private dataApiService: DataApiService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.dataApiService.getHopitalizedByLatest().subscribe((res) => {
+      res = res.reverse();
+      res.forEach((value) => {
+        const date = new Date(value.תאריך);
+        this.lightSick.push({ x: date.getTime(), y: value['חולים קל'] });
+        this.mediumSick.push({
+          x: date.getTime(),
+          y: parseInt(value['חולים בינוני'] + ''),
+        });
+        this.harshSick.push({
+          x: date.getTime(),
+          y: parseInt(value['חולים קשה'] + ''),
+        });
+      });
+      this.updateFlag = true;
+    });
+  }
 }
